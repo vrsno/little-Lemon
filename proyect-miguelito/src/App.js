5b309416f4c4575da2c2585cde054b9d7c95f304
sinname <nexxuz2017@gmail.com>
2024-11-13 12:50:03 -0500

    nevas carpetas y mas cambios

--- proyect-miguelito/src/App.js
+++ proyect-miguelito/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from "./componentes/Nav"
+
+import Main from "./componentes/Main"
+import Footer from "./componentes/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <p>Home Page</p>
+    <div>
+    <React.Fragment>
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
     </div>
   );
 }
