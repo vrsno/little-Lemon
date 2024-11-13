cac999ab074946d5116b42cf8bf30bbce2cb48f0
sinname <nexxuz2017@gmail.com>
2024-11-13 14:08:44 -0500

    aaa

--- proyect-miguelito/src/App.js
+++ proyect-miguelito/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './App.css';
-import Header from './componentes/Header';
-import Nav from './componentes/Nav.js';
-import Main from "./componentes/Main"
-import Footer from "./componentes/Footer"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./componentes/Main";  // El componente que contiene la lógica de tus rutas
+import Header from "./componentes/Header";
+import Booking from "./componentes/Booking";
+import ConfirmedBooking from "./componentes/ConfirmedBooking";
 
-function App() {
+const App = () => {
   return (
-    <div>
-    <React.Fragment>
-      <Nav />
-      <Main />
-      <Footer />
-    </React.Fragment>
-    </div>
+    <Router> {/* Asegúrate de envolver toda tu aplicación con Router */}
+      <div>
+        <Header />
+        <Routes>
+          {/* Define las rutas principales aquí */}
+          <Route path="/" element={<Main />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/confirmed" element={<ConfirmedBooking />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
